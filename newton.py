43c224008a91e56c3cc4ab6d706adcdd95db5a95
Brian Fernando <bmfernan@berkeley.edu>
2024-08-22 13:21:22 -0700

    fixed newton.py to make more robust

--- newton.py
+++ newton.py
@@ -1,19 +1,23 @@
 def function(x):
     return float(x**2) + 10
-    
-epsilon=0.0000005
+
+
+epsilon = 0.0000005
+
+
 def derivative(x):
-    return float((function(x + epsilon) - function(x))/epsilon)
-    
+    return float((function(x + epsilon) - function(x)) / epsilon)
+
+
 def newton_method(x, func):
     x_t_1 = x
     x_t = 0
     while True:
         d1 = derivative(x)
         print(d1)
-        d2 = (derivative(x + epsilon) - derivative(x))/ epsilon
+        d2 = (derivative(x + epsilon) - derivative(x)) / epsilon
         print(d2)
-        x_t = x_t_1 - (d1/d2)
+        x_t = x_t_1 - (d1 / d2)
         print(x_t)
         if abs(x_t - x_t_1) < 6:
             break
@@ -21,6 +25,4 @@ def newton_method(x, func):
     return x_t
 
 
-    
-    
 newton_method(5, function)
