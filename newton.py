cb34f1d4a7400115d81cc47e5006d0a9862f3fa9
Brian Fernando <bmfernan@berkeley.edu>
2024-08-22 13:24:46 -0700

    added doc string to each function

--- newton.py
+++ newton.py
@@ -1,28 +1,26 @@
-def function(x):
-    return float(x**2) + 10
-
-
-epsilon = 0.0000005
-
-
-def derivative(x):
-    return float((function(x + epsilon) - function(x)) / epsilon)
-
-
-def newton_method(x, func):
+def derivative(x, func, epsilon=0.00005):
+    """
+    Calculates the derivative of a function using the epsilon definition of a derivation
+    """
+    return float((func(x + epsilon) - func(x))/epsilon)
+    
+def newton_method(x, func, epsilon=0.00005):
+    """
+    Finds the minimum of function using Newton's Method
+    """
     x_t_1 = x
     x_t = 0
     while True:
-        d1 = derivative(x)
-        print(d1)
-        d2 = (derivative(x + epsilon) - derivative(x)) / epsilon
-        print(d2)
-        x_t = x_t_1 - (d1 / d2)
-        print(x_t)
-        if abs(x_t - x_t_1) < 6:
+        d1 = derivative(x, func)
+        d2 = (derivative(x + epsilon, func) - derivative(x, func))/ epsilon
+        x_t = x_t_1 - (d1/d2)
+        if abs(x_t - x_t_1) < func(x):
             break
         x_t_1 = x_t
     return x_t
 
+# def function(x):
+#     return float((x-10)**2+10)
 
+function = lambda x: (x-2)**2
 newton_method(5, function)
